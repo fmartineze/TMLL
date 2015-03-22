8f2e7a3c4758e1352be50b51c9e8f31d29c937e4
pcpractico <fmartinez@pcpractico.es>
2015-03-22 14:25:47 +0100

    Basic Concept

---
+++ tmll.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMLL
+{
+    class Languages
+    {
+        public string Id { get; set; }
+        public string language_name { get; set; }
+        public string filename { get; set; }
+        public Languages(string id, string language_name, string filename)
+        {
+            this.Id = Id;
+            this.language_name = language_name;
+            this.filename = filename;
+        }
+    }
+    class tmll
+    {
+        private string folder;
+        public Languages LanguagesList;
+
+        public tmll(string folder_name)
+        {
+
+            if (Directory.Exists(folder_name))
+            {
+                folder = folder_name;
+                foreach (string ficheros in Directory.GetFiles(folder_name, "*.lang")) //Catalog all language files
+                {
+                    // insert into a list
+                }
+
+            }
+        }
+
+
+    }
+}
